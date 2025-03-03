057981b3522d13a5579c052f9a94940e3e6cb1fa
Ally Guo <asguo@usc.edu>
2025-03-02 21:47:08 -0800

    added buttons to Rubiks Cube prefabs

--- Game/ShootIX/Assets/Scripts/Piece.cs
+++ Game/ShootIX/Assets/Scripts/Piece.cs
@@ -5,9 +5,14 @@ public class Piece : MonoBehaviour
     [SerializeField] private Color color = Color.white;
     private Face face;
 
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         face = transform.parent.gameObject.GetComponent<Face>();
+
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.color = color;
     }
 
     private void AddToCenterFace()
